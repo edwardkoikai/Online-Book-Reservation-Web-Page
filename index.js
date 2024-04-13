4421f3f785d0ab174ad24a3beb79a8ef1fc0bfdd
Edward Koikai <edwardkoikai@gmail.com>
2024-04-13 23:06:51 +0300

    add style to wishlist container

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-
-function displayBooks(book){
-    let card = document.createElement("li")
-    card.className = "card";
-    let remainingBooks = book.inventory - book.books_sold;
-    card.innerHTML = `
+const scroll = new SmoothScroll('.navBar a[href*="#"]', {
+    speed: 800
+});
+//function to retrieve book details from the json server
+function getAllBooks() {
+    fetch("http://localhost:3000/books")
+      .then((res) => res.json())
+      .then((bookData) => bookData.forEach((book) => displayBooks(book)))
+      .catch((error) => console.log(error));
+  }
+function displayBooks(book) {
+  let card = document.createElement("li");
+  card.className = "card";
+  let remainingBooks = book.inventory - book.books_sold;
+  card.innerHTML = `
     <img src = "${book.cover}">
     <div class= "content">
     <h4>Title:${book.title}</h4>
@@ -18,75 +27,65 @@ function displayBooks(book){
   <div class="wishlistBtn">
     <button id="wishlistBtn">Wishlist <img src="https://icons.veryicon.com/png/o/commerce-shopping/fine-edition-mall-icon/wishlist-1.png"></button>
   </div>
-</div>
+  </div>
+    `;
+  document.querySelector("#book-list").appendChild(card);
 
-    `
-    document.querySelector("#book-list").appendChild(card);
-
-    // Attach event listener to the reserve button
-    card.querySelector('.rsvBtn').addEventListener('click', () => {
-        let copiesLeftParagraph = card.querySelector('.copies-left');
-        reserveBook(card, copiesLeftParagraph);
-    }); 
-    card.querySelector('.wishlistBtn').addEventListener('click', () => {
-        addToWishlist(book);
-    });
-    
+  // Attach event listener to the reserve button
+  card.querySelector(".rsvBtn").addEventListener("click", () => {
+    let copiesLeftParagraph = card.querySelector(".copies-left");
+    reserveBook(card, copiesLeftParagraph);
+  });
+  //Attach event listener to the wishlist button
+  card.querySelector(".wishlistBtn").addEventListener("click", () => {
+    addToWishlist(book);
+  });
 }
+//Function to handle the reserve book button
 function reserveBook(card, copiesLeftParagraph) {
-    let remainingBooks = parseInt(copiesLeftParagraph.textContent.split(': ')[1]);
-    
-    if (remainingBooks > 0) {
-        remainingBooks -= 1;
-        copiesLeftParagraph.textContent = `Copies Left: ${remainingBooks}`;
-        // console.log('Book reserved!');
-        if (remainingBooks === 0) {
-            card.querySelector('.rsvBtn').textContent = 'Not Available';
-        }
-    } else {
-        alert('Book not in stock');
+  let remainingBooks = parseInt(copiesLeftParagraph.textContent.split(": ")[1]);
+
+  if (remainingBooks > 0) {
+    remainingBooks -= 1;
+    copiesLeftParagraph.textContent = `Copies Left: ${remainingBooks}`;
+    // console.log('Book reserved!');
+    if (remainingBooks === 0) {
+      card.querySelector(".rsvBtn").textContent = "Not Available";
     }
-}
-function getAllBooks(){
-    fetch("http://localhost:3000/books")
-    .then(res => res.json())
-    .then(bookData => bookData.forEach(book => displayBooks(book)))
-    .catch(error => console.log(error))
-}
-let searchTitle = document.getElementById("search")
-let searchButton = document.getElementById("searchBtn")
-document.querySelector("#searchBtn").addEventListener("click", () =>{
-    // e.preventDefault()
-    const searchResult = searchTitle.value
-    findBookTitle(searchResult)
-})
-function findBookTitle(searchResult){
-    const allBooks = document.querySelectorAll(".card");
-    for(const item of allBooks){
-        const bookTitle = item.querySelector("h4").innerText.toLowerCase();
-        if(bookTitle.includes(searchResult.toLowerCase())){
-            item.style.display = "inline-block"
-        }
-        else{
-            item.style.display = "none"
-        }
-    }
-}
-function addToWishlist(book) {
-    let basketList = document.querySelector("#basket-list")
-    let basketItem = document.createElement("li")
-    basketItem.className ="card"
-    basketItem.id = "basketItem"
-    
-    basketList.appendChild(basketItem)
-    basketList.querySelector(".rmvBtn").addEventListener("click", deleteWishListItem)
+  } else {
+    alert("Book not in stock");
+  }
 }
 
+
+//attaching event listener to the search button
+let searchTitle = document.getElementById("search");
+let searchButton = document.getElementById("searchBtn");
+document.querySelector("#searchBtn").addEventListener("click", () => {
+  // e.preventDefault()
+  const searchResult = searchTitle.value;
+  findBookTitle(searchResult);
+  searchTitle.value = "";
+});
+//Function for handling search button
+function findBookTitle(searchResult) {
+  const allBooks = document.querySelectorAll(".card");
+  for (const item of allBooks) {
+    const bookTitle = item.querySelector("h4").innerText.toLowerCase();
+    if (bookTitle.includes(searchResult.toLowerCase())) {
+      item.style.display = "inline-block";
+    } else {
+      item.style.display = "none";
+    }
+  }
+}
+
+//Function for handling the wishlist button
 function addToWishlist(book) {
-    let basketList = document.querySelector("#basket-list");
-    let basketItem = document.createElement("li");
-    basketItem.className = "card";
-    basketItem.innerHTML = `
+  let basketList = document.querySelector("#basket-list");
+  let basketItem = document.createElement("li");
+  basketItem.className = "card";
+  basketItem.innerHTML = `
         <img src="${book.cover}">
         <div class="content">
             <h4>Title: ${book.title}</h4>
@@ -98,20 +97,36 @@ function addToWishlist(book) {
             </div>
         </div>
     `;
-    basketList.appendChild(basketItem);
-    basketItem.querySelector(".remove-button").addEventListener("click", deleteWishListItem);
+    
+  basketList.appendChild(basketItem);
+  // attach event listener to the remove button
+  basketItem
+    .querySelector(".remove-button")
+    .addEventListener("click", deleteWishListItem);
 }
 
-function deleteWishListItem(e){
-    e.target.closest('.card').remove(); // Remove the whole basket item
+function deleteWishListItem(e) {
+  e.target.closest(".card").remove(); // Remove the whole basket item
 }
-
-
-function initialize(){
-    getAllBooks();
+// Function for handling the feedback form submit event
+function handleFeedBackForm(event) {
+  event.preventDefault();
+  const feedbackForm = document.getElementById("feedbackForm");
+  const feedbackTextarea = document.getElementById("feedback");
+  const feedbackValue = feedbackTextarea.value;
+  if (feedbackValue.trim() === "") {
+    alert("Please enter your feedback");
+  } else {
+    alert("Feedback Submited");
+    console.log("Feedback submitted:", feedbackValue);
+    feedbackForm.reset();
+  }
+}
+// attach an event listener to the submit form button
+document.querySelector("form").addEventListener("submit", handleFeedBackForm);
+// function for initializing
+function initialize() {
+  getAllBooks();
 }
 
 initialize();
-
-   
-
